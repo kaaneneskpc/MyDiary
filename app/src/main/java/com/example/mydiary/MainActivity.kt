aef130e7a1d5fc5bbf3f2bcc4b4d09822f511c79
INNOVA\kkapici <kkapici@innova.com.tr>
2023-09-21 22:47:37 +0300

    Splash Screen
    - Adding App Icon
    - Creating Splash Screen
    - Adding layout, themes, strings and drawables

--- app/src/main/java/com/example/mydiary/MainActivity.kt
+++ app/src/main/java/com/example/mydiary/MainActivity.kt
@@ -3,11 +3,13 @@ package com.example.mydiary
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.example.mydiary.ui.theme.MydiaryTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         setContent {
             MydiaryTheme {
 
